605d2bcce50b6e24ca7bf31b26ae36d3a8d9c21b
Vlad <jour.vlad@mail.ru>
2016-11-11 04:38:44 +0300

     

--- Rempolimer/js/common.js
+++ Rempolimer/js/common.js
@@ -1,65 +1,99 @@
-ymaps.ready(init); 
-        var myMap;
-        function init () { 
-            myMap = new ymaps.Map("map", {
-                    center: [55.79116633786059, 49.1674262066814], 
-                    zoom: 11,
-                });
-          /* Создаем кастомные метки */
-          myPlacemark0 = new ymaps.Placemark([55.79116633786059, 49.1674262066814], { // Создаем метку с такими координатами и суем в переменную
-                  balloonContent: '<div>Заголовок метки 1<br/><p>Адрес:</p></div>' // сдесь содержимое балуна в формате html, все стили в css
-                }, {
-                iconImageHref: 'img/mark.png', // картинка иконки
-                iconImageSize: [64, 64], // размер иконки
-                iconImageOffset: [-32, -64], // позиция иконки
-                  });
-              /* тоже самое для других меток */
-          myPlacemark1 = new ymaps.Placemark([55.85977356499046,49.18527898988449], {
-                  balloonContent: '<div>Заголовок метки 2<br/><p>Адрес:</p></div>'
-                }, {
-                iconImageHref: 'img/mark2.png',
-                iconImageSize: [64, 64],
-                iconImageOffset: [-32, -64],
-                  
-                  });
-          myPlacemark2 = new ymaps.Placemark([55.83398763747164,49.08932028016772], {
-                  balloonContent: '<div>Заголовок метки 3<br/><p>Адрес:</p></div>'
-                }, {
-                iconImageHref: 'img/mark2.png',
-                iconImageSize: [64, 64],
-                iconImageOffset: [-32, -64],
-                  });
-          myPlacemark3 = new ymaps.Placemark([55.763011919848,49.14974508485522], {
-                  balloonContent: '<div>Заголовок метки 4<br/><p>Адрес:</p></div>'
-                }, {
-                iconImageHref: 'img/mark2.png',
-                iconImageSize: [64, 64],
-                iconImageOffset: [-32, -64],
-                  });
-          myPlacemark4 = new ymaps.Placemark([55.76862508328107,49.25583181581224], {
-                  balloonContent: '<div>Заголовок метки 5<br/><p>Адрес:</p></div>'
-                }, {
-                iconImageHref: 'img/mark2.png',
-                iconImageSize: [64, 64],
-                iconImageOffset: [-32, -64],
-                  });
-          /* Добавляем */
-          myMap.geoObjects
-            .add(myPlacemark0)
-            .add(myPlacemark1)
-            .add(myPlacemark2)
-            .add(myPlacemark3)
-            .add(myPlacemark4);
-
-          /* Фикс кривого выравнивания кастомных балунов */
-      myMap.geoObjects.events.add([
-            'balloonopen'
-        ], function (e) {
-            var geoObject = e.get('target');
-            myMap.panTo(geoObject.geometry.getCoordinates(), {
-                                        delay: 0
-                                    });
-        });
+$(function() {
+var LocsA = [
+    {
+        lat: 12.58,
+        lon: 77.38,
+        title: 'АВИОМ',
+        info: '<div><div class="address col-md-6"><h3>АВИОМ</h3><p>Описание</p><h3>Адрес:</h3><p>Казань, ул. Авиастроительная, дом 50, офис 4</p></div><div class="tel-email col-md-6"><h3>Телефон:</h3><p>+7 (495) 510-87-19</p><h3>E-mail:</h3><a href="#">info@rempolimer.ru</a></div></div>',
+        zoom: 11,
+        icon: 'https://zhuravlev-vlad.github.io/Rempolimer/img/mark.png',
+        animation: google.maps.Animation.DROP
+    },{
+        lat: 18.975,
+        lon: 72.8258,
+        title: 'Дистрибьютер 2',
+        info: '<div><div class="address col-md-6"><h3>Адрес 2</h3><p>Описание</p><h3>Адрес:</h3><p>Казань, ул. Авиастроительная, дом 50, офис 4</p></div><div class="tel-email col-md-6"><h3>Телефон:</h3><p>+7 (495) 510-87-19</p><h3>E-mail:</h3><a href="#">info@rempolimer.ru</a></div></div>',
+        zoom: 11,
+        icon: 'https://zhuravlev-vlad.github.io/Rempolimer/img/mark.png',
+        animation:google.maps.Animation.DROP
+    },{
+        lat: 30.975,
+        lon: 72.8258,
+        title: 'Дистрибьютер 3',
+        info: '<div><div class="address col-md-6"><h3>Адрес 2</h3><p>Описание</p><h3>Адрес:</h3><p>Казань, ул. Авиастроительная, дом 50, офис 4</p></div><div class="tel-email col-md-6"><h3>Телефон:</h3><p>+7 (495) 510-87-19</p><h3>E-mail:</h3><a href="#">info@rempolimer.ru</a></div></div>',
+        zoom: 11,
+        icon: 'https://zhuravlev-vlad.github.io/Rempolimer/img/mark.png',
+        animation:google.maps.Animation.DROP
     }
+];
+var LocsB = [
+    {
+        lat: 12.58,
+        lon: 77.38,
+        title: 'АВИОМ',
+        info: '<div><div class="address col-md-6"><h3>АВИОМ</h3><p>Описание</p><h3>Адрес:</h3><p>Казань, ул. Авиастроительная, дом 50, офис 4</p></div><div class="tel-email col-md-6"><h3>Телефон:</h3><p>+7 (495) 510-87-19</p><h3>E-mail:</h3><a href="#">info@rempolimer.ru</a></div></div>',
+        zoom: 11,
+        icon: 'https://zhuravlev-vlad.github.io/Rempolimer/img/mark.png',
+        animation: google.maps.Animation.DROP
+    },{
+        lat: 18.975,
+        lon: 72.8258,
+        title: 'Дистрибьютер 2',
+        info: '<div><div class="address col-md-6"><h3>Адрес 2</h3><p>Описание</p><h3>Адрес:</h3><p>Казань, ул. Авиастроительная, дом 50, офис 4</p></div><div class="tel-email col-md-6"><h3>Телефон:</h3><p>+7 (495) 510-87-19</p><h3>E-mail:</h3><a href="#">info@rempolimer.ru</a></div></div>',
+        zoom: 11,
+        icon: 'https://zhuravlev-vlad.github.io/Rempolimer/img/mark.png',
+        animation:google.maps.Animation.DROP
+    },{
+        lat: 30.975,
+        lon: 72.8258,
+        title: 'Дистрибьютер 3',
+        info: '<div><div class="address col-md-6"><h3>Адрес 2</h3><p>Описание</p><h3>Адрес:</h3><p>Казань, ул. Авиастроительная, дом 50, офис 4</p></div><div class="tel-email col-md-6"><h3>Телефон:</h3><p>+7 (495) 510-87-19</p><h3>E-mail:</h3><a href="#">info@rempolimer.ru</a></div></div>',
+        zoom: 11,
+        icon: 'https://zhuravlev-vlad.github.io/Rempolimer/img/mark.png',
+        animation:google.maps.Animation.DROP
+    }
+];
+new Maplace({
+    locations: LocsA,
+    map_div: '#gmap-tabs1',
+    controls_div: '#el1-1',
+    controls_type: 'list',
+    controls_on_map: false,
+    show_infowindow: false,
+    start: 1,
+    afterShow: function(index, location, marker) {
+        $('#info').html(location.info);
+    }
+}).Load();
+new Maplace({
+    locations: LocsB,
+    map_div: '#gmap-tabs2',
+    controls_div: '#el2-1',
+    controls_type: 'list',
+    controls_on_map: false,
+    show_infowindow: false,
+    start: 1,
+    afterShow: function(index, location, marker) {
+        $('#info').html(location.info);
+    }
+}).Load();
+});
 
-    
+
+$(document).on('click', '.panel-title-link', function() {
+    var id = 'gmap-tabs' + $(this).attr('data-foo')
+    $('#' + id).addClass('dispblock').removeClass('display').siblings().addClass('display').removeClass('dispblock')
+})
+
+$('.panel-title-link').click(function(){
+    $('.test').toggle({
+        complete:function (){
+            google.maps.event.trigger(Maplace, 'resize');
+        },
+        duration:'slow'
+    });
+})
+
+
+
+
