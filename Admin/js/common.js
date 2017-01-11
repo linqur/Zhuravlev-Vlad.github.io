e1dd9757f3efb373c21b1cde5c77f5ea01aad718
zhuravlev-vlad <jour.vlad@gmail.com>
2017-01-11 17:19:30 +0300

    -

--- Admin/js/common.js
+++ Admin/js/common.js
@@ -1,6 +1,24 @@
 $(function () {
 $("[rel='tooltip']").tooltip();
 });
-
+$(function () {
+$("[data-toggle='popover']").popover();
+});
 $('input[type=file]').bootstrapFileInput();
 $('.file-inputs').bootstrapFileInput();
+
+$("#checkAll").click(function () {
+     $('.check-name input:checkbox').not(this).prop('checked', this.checked);
+ });
+
+
+// cl = console.log;
+
+// $(".table-hover tbody tr").click(function(){
+// 	if ($(this).hasClass('greenGreen')) {
+// 		$(this).removeClass('greenGreen');
+// 	} else {
+// 		$(this).addClass('greenGreen');
+// 	}
+// });
+
