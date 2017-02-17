99457ff34d7b3af7d98488fac77d4b4e6b34ed4f
zhuravlev-vlad <jour.vlad@gmail.com>
2017-02-17 14:18:34 +0300

    -

--- Magic-Service/js/common.js
+++ Magic-Service/js/common.js
@@ -96,35 +96,12 @@ $('.dropdown-menu.subcategory').on('click', function(event){
 });
 
 $('.order_link a').click(function(){
-
   allOrderLink = $( this ).parents('.order_link').find('a'),
   index = $.inArray( this, allOrderLink );
-
-  console.log(index);
   $('.order_link li').removeClass();
   $('.order_link li:nth-child('+(index) +')').addClass('active-neighbor');
   $('.order_link li:nth-child('+(index + 2) +')').addClass('active-neighbor');
 
   $('.order_link li:nth-child('+(index - 1) +')').addClass('active-neighbor-neighbor');
   $('.order_link li:nth-child('+(index + 3) +')').addClass('active-neighbor-neighbor');
-
-});
-$(document).ready(function() {
-  $(form0).find('input').each(function() {
-    var input = $(this);
-    var label = input.parent().children('label'); if(label.length==0) return;
-    
-    if(label.hasClass('valid')) label.removeClass('valid');
-    if(!label.hasClass('animate')) label.addClass('animate');
-    
-    label.click(function() { input.focus(); });
-    input.on('keyup focus blur', event);
-  });
-
-  function event(e) {
-    e = e || window.event;
-    var label = $(this).parent().children('label'); if(label.length==0) return;
-    if($(this).val().length==0 && label.hasClass('valid')) label.removeClass('valid');
-    else if($(this).val().length>0 && !label.hasClass('valid')) label.addClass('valid');
-  }
 });
