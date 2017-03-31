7846968c460426cc5a44cc8141cf648365775487
Zhuravlev Vlad <jour.vlad@gmail.com>
2017-03-31 19:44:01 +0300

    -

--- Magic-Service/js/common.js
+++ Magic-Service/js/common.js
@@ -227,3 +227,28 @@ function stikySidebar(){
     }
 };
 //END stikySidebar
+
+//formLabel
+function formLabel(){
+	$('.labelFormJs').find('input').each(function() {
+	    var input = $(this);
+	    var label = input.parent().children('label'); if(label.length==0) return;
+	    
+	    if(label.hasClass('valid')) label.removeClass('valid');
+	    if(!label.hasClass('animate')) label.addClass('animate');
+	    
+	    label.click(function() { input.focus(); });
+	    input.on('keyup focus blur', event);
+	});
+	
+	function event(e) {
+	    e = e || window.event;
+	    var label = $(this).parent()
+						   .children('label');
+		
+		if(label.length==0) return;
+	    if($(this).val().length==0 && label.hasClass('valid')) label.removeClass('valid');
+	    else if($(this).val().length>0 && !label.hasClass('valid')) label.addClass('valid');
+	}
+};
+//END formLabel
