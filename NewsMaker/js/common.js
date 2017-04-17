7a0623e1afc5e086ef0b6c24fc4499a64973769b
Zhuravlev Vlad <jour.vlad@gmail.com>
2017-04-17 21:27:30 +0300

    -

--- NewsMaker/js/common.js
+++ NewsMaker/js/common.js
@@ -19,6 +19,10 @@ $(document).ready(function() {
 	$(".js-select").select2({
 		minimumResultsForSearch: -1
 	});
+	$(".js-select-modalBet").select2({
+		minimumResultsForSearch: -1,
+		placeholder: "Выберите вариант"
+	});
 });
 $('.js-select-teg').select2({
 	multiple: true,
@@ -336,4 +340,32 @@ function stikySidebar(){
     }
 };
 //END stikySidebar
+// Фиксирует категории
+var StickyElement = function(node){
+  var doc = $(document), 
+      fixed = false,
+      anchor = node.find('.sticky-anchor'),
+      content = node.find('.sticky-content');
+  
+  var onScroll = function(e){
+    var docTop = doc.scrollTop(),
+        anchorTop = anchor.offset().top;
+    if(docTop > anchorTop){
+      if(!fixed){
+        anchor.height(content.outerHeight());
+        content.addClass('fixed');        
+        fixed = true;
+      }
+    }  else   {
+      if(fixed){
+        anchor.height(0);
+        content.removeClass('fixed'); 
+        fixed = false;
+      }
+    }
+  };
+  
+  $(window).on('scroll', onScroll);
+};
+//END Фиксирует категории
 
