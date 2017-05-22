71181ef9ae9fa5fd787685345e691de3ffc7f6b9
Zhuravlev Vlad <jour.vlad@gmail.com>
2017-05-22 17:26:33 +0300

    -

--- travel/js/common.js
+++ travel/js/common.js
@@ -5,17 +5,17 @@ $(document).ready(function() {
 
 	var sidebarOpen   = $('.sidebar--open'),
 		sidebarClose  = $('.sidebar--close'),
-		sidebarTag    = $('.sidebarTag'),
+		sidebar    	  = $('.sidebar'),
 		hotelCategory = $('.hotelCategory-itm--js'),
 		hotelFood	  = $('.hotelFood-itm--js'),
 		adult 		  = $('.adult-itm'),
 		child 		  = $('.child-itm');
 
 	sidebarOpen.click(function(){
-		sidebarTag.addClass('sidebarTag--show');
+		sidebar.addClass('sidebar--show');
 	});
 	sidebarClose.on('click', function(){
-		sidebarTag.removeClass('sidebarTag--show');
+		sidebar.removeClass('sidebar--show');
 	});
 
 
@@ -130,7 +130,78 @@ $(document).ready(function() {
 	
 	});
 //END range
+	
+	$('#mainSlider').slick({
+		infinite: true,
+		slidesToShow: 2,  
+		slidesToScroll: 1,
+		variableWidth: true,
+		prevArrow: false,
+      	nextArrow: $('.slick-next'),
+		responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			}
+		}] 
+	});
 
+	
+	function resizeSlider(){
+		var widthSlider = $('.mainSlider').width(),
+			mainSliderItm = $('.mainSlider-itm');
+
+		if (widthSlider > 768){
+			mainSliderItm.css('width', widthSlider * 0.41 + 'px')
+		} else
+		if (widthSlider > 480){
+			mainSliderItm.css('width', widthSlider * 0.7 + 'px')
+		}
+
+		var k = 606/500
+		mainSliderItm.height($('.mainSlider-itm').width() / k);
+		$('.slick-next').height($('.mainSlider-itm').width() / k);
+	};
+	
+	$( window ).resize(resizeSlider);
+	$( document ).ready(resizeSlider);
+	
+	$('.tabNavigation a').click(function(){
+		$('.tabNavigation a').removeClass('active');
+		$(this).addClass('active');
+	});
+	
+//	var countType = $('.tabNavigation a').size(),
+//    	step = 100 / countType;
+	
+//	var widthListPr = 0;
+//	$('.tabNavigation li').each(function(){ 
+//		widthListPr = widthListPr + $(this).width() 
+//	});
+//	console.log(widthListPr);
+
+//	var summ=0;
+//	$('tabNavigation li').each(function(){
+//		summ += 1*$(this).width();
+//	});
+
+	$('.tabNavigation li').click(function () {
+		var widthLink = $(this).width(),
+			summ = 0,
+			step = $(this).prevAll().each(function () {
+				summ += 4 + 1 * $(this).outerWidth(true);
+			});
+
+		$('.tabNavigation a')
+			.parents('.tabNavigation')
+			.find('hr')
+			.css('margin-left', summ + 'px');
+
+		$('.tabNavigation hr').css('width', widthLink + 'px')
+	});
 
 });
 
@@ -141,6 +212,16 @@ $(document).ready(function() {
 			theme: 'dark-thick',
 			scrollbarPosition: 'outside'
 		});
+		
+		var doc_w = $(document).width();
+
+		if (doc_w > 768){
+			$('.scroll').mCustomScrollbar({
+				scrollButtons: {enable: false},
+				theme: 'minimal-dark',
+				//scrollbarPosition: 'outside'
+			});
+		}
 	});
 })(jQuery);
 
@@ -149,52 +230,83 @@ $(document).ready(function() {
 
 
 
-//stikySidebar function(){
-//	Array.prototype.slice.call(document.querySelectorAll('#aside1, #aside2')).forEach(function(a) { 
-//	var b = null, P = 0;
-//	window.addEventListener('scroll', Ascroll, false);
-//	document.body.addEventListener('scroll', Ascroll, false);
-//	function Ascroll() {
-//	  if (b == null) {
-//		var Sa = getComputedStyle(a, ''), s = '';
-//		for (var i = 0; i < Sa.length; i++) {
-//		  if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-//			s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-//		  }
-//		}
-//		b = document.createElement('div');
-//		b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-//		a.insertBefore(b, a.firstChild);
-//		var l = a.childNodes.length;
-//		for (var i = 1; i < l; i++) {
-//		  b.appendChild(a.childNodes[1]);
-//		}
-//		a.style.height = b.getBoundingClientRect().height + 'px';
-//		a.style.padding = '0';
-//		a.style.border = '0';
-//	  }
-//	  var Ra = a.getBoundingClientRect(),
-//		  R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('#article').getBoundingClientRect().bottom + 0);
-//	  if ((Ra.top - P) <= 0) {
-//		if ((Ra.top - P) <= R) {
-//		  b.className = 'stop';
-//		  b.style.top = - R +'px';
-//		  b.style.left = 0;
-//		} else {
-//		  b.className = 'sticky';
-//		  b.style.top = P + 'px';
-//		  b.style.left = Ra.left + 'px';
-//		}
-//	  } else {
-//		b.className = '';
-//		b.style.top = '';
-//		b.style.left = '';
-//	  }
-//	  window.addEventListener('resize', function() {
-//		a.children[0].style.width = getComputedStyle(a, '').width;
-//		b.style.left = (b.className == 'sticky' ? (a.getBoundingClientRect().left + 'px') : '0');
-//	  }, false);
-//	}
-//	})
-//};
-//stikySidebar();
+(function(){
+var a = document.querySelector('#aside1'), b = null, K = null, Z = 0, P = 0, N = 0;  // если у P ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента, если у N — нижний край дойдёт до нижнего края элемента. Может быть отрицательным числом
+window.addEventListener('scroll', Ascroll, false);
+document.body.addEventListener('scroll', Ascroll, false);
+function Ascroll() {
+  var Ra = a.getBoundingClientRect(),
+      R1bottom = document.querySelector('#article').getBoundingClientRect().bottom;
+  if (Ra.bottom < R1bottom) {
+    if (b == null) {
+      var Sa = getComputedStyle(a, ''), s = '';
+      b = document.createElement('div');
+      b.className = "stop";
+      b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+      a.insertBefore(b, a.firstChild);
+      var l = a.childNodes.length;
+      for (var i = 1; i < l; i++) {
+        b.appendChild(a.childNodes[1]);
+      }
+      a.style.height = b.getBoundingClientRect().height + 'px';
+      a.style.padding = '0';
+      a.style.border = '0';
+    }
+    var Rb = b.getBoundingClientRect(),
+        Rh = Ra.top + Rb.height,
+        W = document.documentElement.clientHeight,
+        R1 = Math.round(Rh - R1bottom),
+        R2 = Math.round(Rh - W);
+    if (Rb.height > W) {
+      if (Ra.top < K) {  // скролл вниз
+        if (R2 + N > R1) {  // не дойти до низа
+          if (Rb.bottom - W + N <= 0) {  // подцепиться
+            b.className = 'sticky';
+            b.style.top = W - Rb.height - N + 'px';
+            Z = N + Ra.top + Rb.height - W;
+          } else {
+            b.className = 'stop';
+            b.style.top = - Z + 'px';
+          }
+        } else {
+          b.className = 'stop';
+          b.style.top = - R1 +'px';
+          Z = R1;
+        }
+      } else {  // скролл вверх
+        if (Ra.top - P < 0) {  // не дойти до верха
+          if (Rb.top - P >= 0) {  // подцепиться
+            b.className = 'sticky';
+            b.style.top = P + 'px';
+            Z = Ra.top - P;
+          } else {
+            b.className = 'stop';
+            b.style.top = - Z + 'px';
+          }
+        } else {
+          b.className = '';
+          b.style.top = '';
+          Z = 0;
+        }
+      }
+      K = Ra.top;
+    } else {
+      if ((Ra.top - P) <= 0) {
+        if ((Ra.top - P) <= R1) {
+          b.className = 'stop';
+          b.style.top = - R1 +'px';
+        } else {
+          b.className = 'sticky';
+          b.style.top = P + 'px';
+        }
+      } else {
+        b.className = '';
+        b.style.top = '';
+      }
+    }
+    window.addEventListener('resize', function() {
+      a.children[0].style.width = getComputedStyle(a, '').width
+    }, false);
+  }
+}
+})()
